f09880c643ed629916df73dd208f61cde48628d8
loren pabst <loren pabst>
2017-04-20 12:09:19 -0600

    added script.js

---
+++ app/scripts/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+var listo = [];
+
+var Task = function(task){
+    this.task = task;
+    this.id = 'new';
+}
+
+
+
+
+
+
+
+
+
+}); // End Jquery document.ready function
